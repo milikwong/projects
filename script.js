0f1509d880a824334fc71f6f7b75005a43c78e12
milikwong <157768286+milikwong@users.noreply.github.com>
2024-01-28 14:12:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,120 @@
+/*
+  This is your site JavaScript code - you can add interactivity!
+*/
+
+// Print a message in the browser's dev tools console each time the page loads
+// Use your menus or right-click / control-click and choose "Inspect" > "Console"
+console.log("Hello 🌎");
+
+/* 
+Make the "Click me!" button move when the visitor clicks it:
+- First add the button to the page by following the steps in the TODO 🚧
+*/
+const btn = document.querySelector("button"); // Get the button from the page
+if (btn) {
+  // Detect clicks on the button
+  btn.onclick = function () {
+    // The 'dipped' class in style.css changes the appearance on click
+    btn.classList.toggle("dipped");
+  };
+}
+
+// ----- GLITCH STARTER PROJECT HELPER CODE -----
+
+// Open file when the link in the preview is clicked
+let goto = (file, line) => {
+  window.parent.postMessage(
+    { type: "glitch/go-to-line", payload: { filePath: file, line: line } },
+    "*"
+  );
+};
+// Get the file opening button from its class name
+const filer = document.querySelectorAll(".fileopener");
+filer.forEach((f) => {
+  f.onclick = () => {
+    goto(f.dataset.file, f.dataset.line);
+  };
+});
+
+// -------"back to top" button---------
+const showOnPx = 100;
+const backToTopButton = document.querySelector(".back-to-top");
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+const goToTop = () => {
+  document.body.scrollIntoView({
+    behavior: "smooth",
+  });
+};
+
+document.addEventListener("scroll", () => {
+  console.log("Scroll Height: ", scrollContainer().scrollHeight);
+  console.log("Client Height: ", scrollContainer().clientHeight);
+
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
+
+backToTopButton.addEventListener("click", goToTop);
+
+// ---------"slideshow-------------
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+
+document.addEventListener("contextmenu", (event) => event.preventDefault());
+
+
+document.addEventListener("keydown", (event) => {
+  if (
+    event.keyCode === 123 ||
+    (event.ctrlKey && event.shiftKey && event.keyCode === 73)
+  ) {
+    event.preventDefault();
+  }
+});
+
+
+document.addEventListener("keydown", function (event) {
+
+  if (event.key === "u" && event.metaKey && event.altKey) {
+    event.preventDefault();
+    console.log("Option + Command + U is disabled.");
+  }
+});
